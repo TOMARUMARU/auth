864aa0053724f30334ef1e0a0d94c2c0e74616a3
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-27 15:46:29 +0900

    add Text for error

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
@@ -38,6 +39,10 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+
+        <Text>
+          {this.state.error}
+        </Text>
 
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
