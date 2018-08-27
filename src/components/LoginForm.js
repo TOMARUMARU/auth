c0eedfa76ce9262af6dfadebcb62c5917e8dd3ad
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-27 13:42:47 +0900

    import TextInput

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
-        <CardSection />
+        <CardSection>
+          <TextInput />
+        </CardSection>
+
         <CardSection />
 
         <CardSection>
