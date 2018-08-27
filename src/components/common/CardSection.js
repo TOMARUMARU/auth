3a859bd3121699f6f65a513d8fbc7defd35e915d
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-27 12:26:30 +0900

    copy 4 js file

---
+++ src/components/common/CardSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+  return (
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+    borderBttomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    positon: 'relative'
+  }
+};
+
+export default CardSection;
