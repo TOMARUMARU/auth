51e94f6f134bd70eea1dd0937e1f9db6750b2437
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-27 13:14:10 +0900

    mount firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import { Header } from './components/common';
 import firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: '¥AIzaSyCBwjeyIGepjzpeLL5ecgqN9u95P4K9N0Y',
+      authDomain: 'authentication-1376d.firebaseapp.com',
+      databaseURL: 'https://authentication-1376d.firebaseio.com',
+      projectId: 'authentication-1376d',
+      storageBucket: 'authentication-1376d.appspot.com',
+      messagingSenderId: '13919341067'
+    });
+  }
+
   render() {
     return (
       <View>
